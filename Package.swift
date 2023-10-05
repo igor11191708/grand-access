91cac4a35bf3119c5eee04b24bb69fc1cf531ea1
Igor <igorshelopaev@gmail.com>
2023-10-05 09:12:03 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "grand-access",
     defaultLocalization: "ru",
-    platforms: [.iOS("16")],
+    platforms: [.iOS("15")],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
